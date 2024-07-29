49ed30389c4e972bd5a7ba381b5bd90f0b6a2df8
anna.masharova <amasharova@magnite.com>
2024-07-29 15:20:53 +0200

    Set up test framework

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sport Radar API Automation Assessment
+Sport Radar Tests repository. Includes Functional tests for reqres.in server.
+
+## Contributors
+
+* **Anna Masharova**  - [masharova.ann2911@gmail.com](mailto:masharova.ann2911@gmail.com)
---
+++ src/main/java/models/user/UserDTO.java
@@ -0,0 +1,16 @@
+package models.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class UserDTO {
+
+    public String email;
+    public String password;
+}
